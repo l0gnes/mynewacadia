1ad88ae4ef29c3cddadea54b9e48801f4db1d38f
l0gnes <loonargt@gmail.com>
2023-11-02 20:09:39 -0300

    Adding sections route for calendar stuff

---
+++ server/api/sections/[id].get.js
@@ -0,0 +1,14 @@
+import { serverSupabaseClient } from "#supabase/server";
+
+export default defineEventHandler(
+    async (event) => {
+        const supa = await serverSupabaseClient(event);
+        const section_id = parseInt(getRouterParam(event, "id"));
+
+        const {data, error} = await supa.from("sections")
+            .select("*, course : courses ( * )")
+            .eq("section_id", section_id);
+
+        return data;
+    }
+)
